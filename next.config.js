feb3e5a154642b2e9646a35c7ba6db2050d21595
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-11-05 20:38:27 +0200

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
-// /* eslint-disable import/no-extraneous-dependencies */
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-
-// module.exports = withBundleAnalyzer({
-//   eslint: {
-//     dirs: ['.'],
-//   },
-//   images: {
-//     domains: ['images.unsplash.com'],
-//   },
-//   poweredByHeader: false,
-//   trailingSlash: true,
-//   basePath: '',
-//   // The starter code load resources from `public` folder with `router.basePath` in React components.
-//   // So, the source code is "basePath-ready".
-//   // You can remove `basePath` if you don't need it.
-//   reactStrictMode: true,
-// });
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
