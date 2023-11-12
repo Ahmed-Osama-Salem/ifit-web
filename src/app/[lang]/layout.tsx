2fa132807505fa5d151119dfa099b5faf7160e3d
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-11-12 23:23:05 +0200

    style(navbarSlide): add navbar silde

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable unused-imports/no-unused-vars */
 import './global.css';
 
 import { Nunito } from 'next/font/google';
--- src/component/modules/Navbar.tsx
+++ src/component/modules/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import type { UserModel } from '@/app/[lang]/page';
@@ -14,6 +15,11 @@ import AuthModal from './auth/AuthModal';
 import Hamburger from './Hamburger';
 
 const Navbar = ({ user }: { user?: UserModel }) => {
+  const [isOpen, setIsOpen] = useState<Boolean>(false);
+
+  const toggleNavbar = (): void => {
+    setIsOpen(!isOpen);
+  };
   // const pathname = usePathname();
   const dispatch = useDispatch();
 
@@ -25,81 +31,111 @@ const Navbar = ({ user }: { user?: UserModel }) => {
   // ];
 
   return (
-    <Container
-      flexDirection="row"
-      bgColor="bg-white"
-      className="fixed top-0 flex h-[87px] w-full items-center justify-between px-[60px]"
-    >
+    <>
       <Container
-        bgColor="none"
         flexDirection="row"
-        className="flex flex-row gap-10"
+        bgColor="bg-white"
+        className="fixed top-0 z-10 flex h-[87px] w-full items-center justify-between px-[60px]"
       >
-        <Hamburger />
-        {/* {navigationTabs.map((el, i) => {
+        <Container
+          bgColor="none"
+          flexDirection="row"
+          className="flex flex-row gap-10"
+        >
+          <Hamburger isOpen={isOpen} toggleNavbar={toggleNavbar} />
+          {/* {navigationTabs.map((el, i) => {
           return (
             <Link key={i as number} href={el.href}>
-              <TypographyText
+            <TypographyText
                 tag="h3"
                 className={`cursor-pointer text-[16px] font-bold  ${
                   pathname === el.href ? 'text-brown-normal' : 'text-[#98A1B3]'
                 }`}
-              >
+                >
                 {el.label}
               </TypographyText>
-            </Link>
+              </Link>
           );
         })} */}
-      </Container>
-      <Container
-        flexDirection="row"
-        bgColor="bg-white"
-        className="flex h-full items-center"
-      >
-        {/* <img src={logo.src} className="h-auto w-[4rem]" alt="ifit" /> */}
-        <Image src={logo.src} alt="ifit" width={64} height={100} />
-        <TypographyText
-          tag="h3"
-          className="translate-x-[-10px] pt-2 text-[36px] font-bold text-brown-normal"
-        >
-          i Fit
-        </TypographyText>
-      </Container>
-      {!user ? (
-        <Button
-          bgType="default"
-          bgClass="btn_main"
-          rounded="rounded-[100px]"
-          padding="px-6 py-2"
-          onClick={() => {
-            dispatch(setPopupName('Auth'));
-            dispatch(showPopup(<AuthModal />));
-          }}
-        >
-          sign in
-        </Button>
-      ) : (
+        </Container>
         <Container
-          bgColor="none"
-          className="flex flex-row items-center justify-center gap-2 text-center"
           flexDirection="row"
+          bgColor="bg-white"
+          className="flex h-full items-center"
         >
-          {/* <h2>{user.name}</h2> */}
-          {/* <img
+          {/* <img src={logo.src} className="h-auto w-[4rem]" alt="ifit" /> */}
+          <Image src={logo.src} alt="ifit" width={64} height={100} />
+          <TypographyText
+            tag="h3"
+            className="translate-x-[-10px] pt-2 text-[36px] font-bold text-brown-normal"
+          >
+            i Fit
+          </TypographyText>
+        </Container>
+        {!user ? (
+          <Button
+            bgType="default"
+            bgClass="btn_main"
+            rounded="rounded-[100px]"
+            padding="px-6 py-2"
+            onClick={() => {
+              dispatch(setPopupName('Auth'));
+              dispatch(showPopup(<AuthModal />));
+            }}
+          >
+            sign in
+          </Button>
+        ) : (
+          <Container
+            bgColor="none"
+            className="flex flex-row items-center justify-center gap-2 text-center"
+            flexDirection="row"
+          >
+            {/* <h2>{user.name}</h2> */}
+            {/* <img
             src={user?.image}
             alt="user_avatar"
             className="h-full w-[50px] rounded-full"
           /> */}
-          <Image
-            src={user?.image}
-            alt="user_avatar"
-            className="h-full w-[50px] rounded-full"
-            width={50}
-            height={100}
-          />
-        </Container>
-      )}
-    </Container>
+            <Image
+              src={user?.image}
+              alt="user_avatar"
+              className="h-full w-[50px] rounded-full"
+              width={50}
+              height={100}
+            />
+          </Container>
+        )}
+      </Container>
+      {isOpen ? (
+        <div className="absolute left-0 top-[87px] h-[90vh] w-full bg-white px-16">
+          <TypographyText
+            tag="h3"
+            className="text-[80px] font-bold text-brown-normal"
+          >
+            My Questions
+          </TypographyText>
+          <TypographyText
+            tag="h3"
+            className="text-[80px] font-bold text-brown-normal"
+          >
+            Articels
+          </TypographyText>{' '}
+          <TypographyText
+            tag="h3"
+            className="text-[80px] font-bold text-brown-normal"
+          >
+            Home
+          </TypographyText>{' '}
+          <TypographyText
+            tag="h3"
+            className="text-[80px] font-bold text-brown-normal"
+          >
+            My Settings
+          </TypographyText>
+        </div>
+      ) : null}
+    </>
   );
 };
 
